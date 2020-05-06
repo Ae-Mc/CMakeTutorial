1d995d4f9cb7627beca20fc44cfded92a2b9ad85
Ae-Mc <ae_mc@mail.ru>
2020-05-07 01:34:20 +0300

    Simple project with standard specification

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+auto sum(int a, int b) {
+    return a + b;
+}
+
 int main() {
     std::cout << "Hello CMake" << std::endl;
+    std::cout << "2 + 3 = " << sum(2, 3) << std::endl;
     return 0;
 }
