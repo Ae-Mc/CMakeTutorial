0f640764fa8bafbf52f88b1dfd4f656e2b7cf101
Ae-Mc <ae_mc@mail.ru>
2020-05-07 02:00:06 +0300

    Add macro definition that used from C++ code to cmake

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#ifndef CMAKEMACROSAMPLE
+    #define CMAKEMACROSAMPLE "NO SYSTEM NAME PROVIDED"
+#endif
 
 auto sum(int a, int b) {
     return a + b;
@@ -6,6 +9,7 @@ auto sum(int a, int b) {
 
 int main() {
     std::cout << "Hello CMake" << std::endl;
+    std::cout << "OS: " << CMAKEMACROSAMPLE << std::endl;
     std::cout << "2 + 3 = " << sum(2, 3) << std::endl;
     return 0;
 }
